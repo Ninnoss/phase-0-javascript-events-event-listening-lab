e2734ed7e7efa7000f6e2d01e72fd9ce52dc285c
Ninos Dinkha <ninos.dinkha97@gmail.com>
2022-10-19 22:53:26 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 function addingEventListener() {
+  const input = document.getElementById('button');
+  input.addEventListener('click', clickAlert);
+}
+
+// calling the function
+addingEventListener();
+
+
+function clickAlert() {
+  alert('I was clicked!');
 }
